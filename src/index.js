6cc5916e9c07b4921e421a47915f38efcc598a6a
Tanner <troxs690@gmail.com>
2022-02-11 14:08:19 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 const init = () => {
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = document.querySelector('input#searchByID');
+    console.log(input.value);
+
+fetch (`http://localhost:3000/movies/${input.value}`)
+.then(resp => resp.json())
+.then(data => {
+  console.log(data)
   
+  let head = document.querySelector('h4')
+  let para = document.querySelector('p')
+  head.innerText = data.title
+  para.innerText = data.summary
+
+
+})
+})
 }
+
+
+
+
+
+
+
+
+
+
+
 
 document.addEventListener('DOMContentLoaded', init);
